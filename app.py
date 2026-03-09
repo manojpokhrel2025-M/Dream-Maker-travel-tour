37e313009d959248a28fe09a34a8a61e37dc16ed
manojpokhrel2025-M <manojpokhrel2025@gmail.com>
2026-03-09 10:55:56 +0530

    Update app.py
    Updated 
    
    import os
    
    if __name__ == "__main__":
        port = int(os.environ.get("PORT", 10000))
        app.run(host="0.0.0.0", port=port)

--- app.py
+++ app.py
@@ -55,6 +55,9 @@ def send_email():
             flash(f'Error: {str(e)}', 'danger')
         
         return redirect(url_for('index') + '#contact')
+        
+import os
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
